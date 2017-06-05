68f3aed6cb8f28fcaf0107bac6fc640455afeef6
antimatter337 <antimatter337@users.noreply.github.com>
2017-06-05 17:28:19 -0500

    Update mitchell.py

--- mitchell.py
+++ mitchell.py
@@ -5,7 +5,7 @@ Scraping http://www.mitchellhogan.com/blog/ for book updates.
 
 Checks for updates and adds a time stamp to show time between changes.
 
-This script  uses 4 text files make sure to create them add
+This script  uses 4 text files make sure to create them and add
 the locations to the per, per-old, new and old varibles.
 """
 
